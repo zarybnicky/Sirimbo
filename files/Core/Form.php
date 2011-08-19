d826537d88151d8035991a3676dcbc1a086f3ef0
JZarybnicky <kuba.zarybnicky@post.cz>
2011-08-19 12:32:15 +0200

    Start on Git and fresh Ubuntu :o)

---
+++ files/Core/Form.php
@@ -0,0 +1,37 @@
+<?php
+function formError() {
+	echo "<span style=\"color:red;\"> !!!</span>";
+}
+function checkPostField($regex, $text) {
+	global $isConfirmation;
+	if($isConfirmation) {
+		if(isset($_POST[$text]) && preg_match($regex, $_POST[$text])) {	
+			return false;
+		} else {
+			formError();
+			return true;
+		}
+	} else {
+		return true;
+	}
+}
+function checkPostFieldLength($minIncl, $maxIncl, $text) {
+	global $isConfirmation;
+	if($isConfirmation) {
+		if(isset($_POST[$text]) && strlen($_POST[$text]) >= $minIncl && strlen($_POST[$text]) <= $maxIncl) {	
+			return false;
+		} else {
+			formError();
+			return true;
+		}
+	} else {
+		return true;
+	}
+}
+function getPostField($field) {
+	if(isset($_POST[$field]))
+		return $_POST[$field];
+	else
+		return null;
+}
+?>
