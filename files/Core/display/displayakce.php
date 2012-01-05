44a5f8bbe23c76dea8390de1dca1b2d9e9556cef
JZarybnicky <kuba.zarybnicky@post.cz>
2012-01-05 22:10:00 +0100

    New design of Akce,Rozpis,Nabidka

--- files/Core/display/displayakce.php
+++ files/Core/display/displayakce.php
@@ -1,18 +1,22 @@
 <?php
 class DisplayAkce {
 	public static function viewAkceHeader($data) {
-		echo '<div class="a_info" style="border:1px solid black;">';
-		echo '<div class="a_jmeno">', $data['a_jmeno'];
-		if(Permissions::canEditAkce())
-			echo ' - <a href="/admin/akce/edit/', $data['a_id'], '">Editovat</a>';
-		echo '</div>';
-	
-		echo '<div class="a_datum">', formatDate($data['a_od']);
+		echo '<div class="trenink_header" style="width:330px;">';
+		echo '<div class="nadpis">', $data['a_jmeno'], '</div>';
+		echo '<div style="letter-spacing:1px;font-weight:bold;">', formatDate($data['a_od']);
 		if($data['a_od'] != $data['a_do'])
 			echo ' - ', formatDate($data['a_do']);
 		echo '</div>';
-		echo '<div class="a_moreinfo">Další informace: ', $data['a_info'], '</div>';
-		echo '<div class="a_dokumenty">Dokumenty:<br/>';
+		
+		if(Permissions::canEditAkce()) {
+			echo '<span class="big">Admin:</span><br/>';
+			echo '<a href="/admin/akce/edit/', $data['a_id'], '">obecné</a>, ';
+			echo '<a href="/admin/akce/detail/', $data['a_id'], '">účastníci</a>, ';
+			echo '<a href="/admin/akce/dokumenty/', $data['a_id'], '">dokumenty</a>';
+		}
+	
+		echo '<div style="text-align:left;"><span class="big">Další informace:</span><br/>', nl2br($data['a_info']), '</div>';
+		echo '<div style="text-align:left;"><span class="big">Dokumenty:</span><br/>';
 		$doku = unserialize($data['a_dokumenty']);
 		if($doku) {
 			foreach($doku as $id) {
@@ -34,14 +38,13 @@ class DisplayAkce {
 		}
 		$akce_items = DBAkce::getAkceItems($id);
 		
-		echo '<div class="a_wrapper">';
-		
 		DisplayAkce::viewAkceHeader($akce);
 		
+		echo '<div style="text-align:center;">';
 		if(empty($akce_items)) {
-			echo 'Nikdo není přihlášený';
+			echo 'Nikdo není přihlášený<br/>';
 		} else {
-			echo '<table>';
+			echo '<table style="margin:0 auto;">';
 			echo '<tr><td>Jméno</td><td>Příjmení</td><td>Rok narození</td></tr>';
 			foreach($akce_items as $par) {
 				echo '<tr><td>', $par['ai_jmeno'], '</td><td>', $par['ai_prijmeni'], '</td><td>',
@@ -49,7 +52,7 @@ class DisplayAkce {
 			}
 			echo '</table>';
 		}
-		echo '</div>';
 		echo '<a href="/member/akce">Zpět</a>';
+		echo '</div>';
 	}
 }
--- files/Core/display/displayrozpis.php
+++ files/Core/display/displayrozpis.php
@@ -5,10 +5,14 @@ class DisplayRozpis {
 		echo '<div class="nadpis">';
 		echoFullJmeno($data);
 		echo '</div>';
-		if(Permissions::canEditRozpis($data['r_id']))
-			echo '<a href="/admin/rozpis/edit/', $data['r_id'], '">Editovat</a>';
-		
 		echo '<div style="letter-spacing:1px;font-weight:bold;">', formatDate($data['r_datum']), '</div>';
+		
+		if(Permissions::canEditRozpis($data['r_id'])) {
+			echo '<span class="big">Admin:</span> ';
+			echo '<a href="/admin/rozpis/edit/', $data['r_id'], '">obecné</a>, ';
+			echo '<a href="/admin/rozpis/detail/', $data['r_id'], '">tréninky</a>';
+		}
+		
 		echo '<div>', $data['r_kde'], '</div>';
 		echo '</div>';
 	}
