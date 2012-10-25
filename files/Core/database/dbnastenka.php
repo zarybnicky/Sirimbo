7bb0d38ef54ec80d75b6fe6596f36bae674d2c37
dradancer <kuba.zarybnicky@post.cz>
2012-10-25 21:11:35 +0200

    Added skupiny - in users and nastenka, select helper, began platby

--- files/Core/database/dbnastenka.php
+++ files/Core/database/dbnastenka.php
@@ -1,12 +1,40 @@
 <?php
 class DBNastenka extends Database {
-	public static function getNastenka() {
+	public static function getNastenka($offset = NULL, $count = NULL) {
 		$res = DBNastenka::query(
 		"SELECT up_id,up_kdo,u_jmeno,u_prijmeni,up_aktu,up_nadpis,up_text,up_barvy,up_lock
 		FROM upozorneni
 			LEFT JOIN users ON up_kdo=u_id
-		ORDER BY up_aktu DESC");
+		ORDER BY up_aktu DESC" . (($offset !== NULL && $count !== NULL) ? " LIMIT $offset,$count" : ''));
 		return DBNastenka::getArray($res);
+	}
+	
+	public static function getNastenkaCount() {
+		$res = DBNastenka::query(
+		"SELECT COUNT(*) FROM upozorneni");
+		$res = DBNastenka::getSingleRow($res);
+		return $res['COUNT(*)'];
+	}
+	
+	public static function getNastenkaSkupiny($id) {
+		list($id) = DBNastenka::escapeArray(array($id));
+		
+		$res = DBNastenka::query("SELECT * FROM upozorneni_skupiny WHERE ups_id_rodic='$id'");
+		return DBNastenka::getArray($res);
+	}
+	
+	public static function addNastenkaSkupina($rodic, $skupina, $color, $popis) {
+		list($rodic, $skupina, $color, $popis) =
+			DBNastenka::escapeArray(array($rodic, $skupina, $color, $popis));
+		
+		DBNastenka::query("INSERT INTO upozorneni_skupiny (ups_id_rodic,ups_id_skupina,ups_color,ups_popis)
+			VALUES ('$rodic','$skupina','$color','$popis')");
+	}
+	
+	public static function removeNastenkaSkupina($id) {
+		list($id) = DBNastenka::escapeArray(array($id));
+		
+		DBNastenka::query("DELETE FROM upozorneni_skupiny WHERE ups_id='$id'");
 	}
 	
 	public static function getNastenkaUserName($id) {
@@ -46,22 +74,22 @@ class DBNastenka extends Database {
 		}
 	}
 	
-	public static function addNastenka($userid, $nadpis, $text, $barvy, $lock) {
-		list($userid, $nadpis, $text, $barvy, $lock) =
-			DBNastenka::escapeArray(array($userid, $nadpis, $text, $barvy, $lock));
+	public static function addNastenka($userid, $nadpis, $text, $lock) {
+		list($userid, $nadpis, $text, $lock) =
+			DBNastenka::escapeArray(array($userid, $nadpis, $text, $lock));
 		
-		DBNastenka::query("INSERT INTO upozorneni (up_kdo,up_nadpis,up_text,up_barvy,up_lock) VALUES " .
-			"('$userid','$nadpis','$text','$barvy','$lock')");
+		DBNastenka::query("INSERT INTO upozorneni (up_kdo,up_nadpis,up_text,up_lock) VALUES " .
+			"('$userid','$nadpis','$text','$lock')");
 		
 		return true;
 	}
 	
-	public static function editNastenka($id, $nadpis, $text, $barvy, $lock) {
-		list($id, $nadpis, $text, $barvy, $lock) =
-			DBNastenka::escapeArray(array($id, $nadpis, $text, $barvy, $lock));
+	public static function editNastenka($id, $nadpis, $text, $lock) {
+		list($id, $nadpis, $text, $lock) =
+			DBNastenka::escapeArray(array($id, $nadpis, $text, $lock));
 		
 		DBNastenka::query("UPDATE upozorneni SET " .
-			"up_nadpis='$nadpis',up_text='$text',up_barvy='$barvy',up_lock='$lock' WHERE up_id='$id'");
+			"up_nadpis='$nadpis',up_text='$text',up_lock='$lock' WHERE up_id='$id'");
 		
 		return true;
 	}
@@ -70,6 +98,7 @@ class DBNastenka extends Database {
 		list($id) = DBNastenka::escapeArray(array($id));
 		
 		DBNastenka::query("DELETE FROM upozorneni WHERE up_id='$id'");
+		DBNastenka::query("DELETE FROM upozorneni_skupiny WHERE ups_id_rodic='$id'");
 		
 		return true;
 	}
---
+++ files/Core/database/dbplatby.php
@@ -0,0 +1,43 @@
+<?php
+class DBPlatby extends Database {
+	public static function getPlatby() {
+		$res = DBPlatby::query("SELECT * FROM users_platby");
+		return DBPlatby::getArray($res);
+	}
+	public static function getSinglePlatba($id) {
+		list($id) = DBPlatby::escapeArray(array($id));
+		
+		$res = DBPlatby::query("SELECT * FROM users_platby WHERE up_id='$id'");
+		return DBPlatby::getSingleRow($res);
+	}
+	public static function addPlatba($user, $castka, $placeno, $plati_do) {
+		list($user, $castka, $placeno, $plati_do) =
+			DBPlatby::escapeArray(array($user, $castka, $placeno, $plati_do));
+		
+		DBPlatby::query("INSERT INTO users_platby
+		(up_id_user,up_castka,up_placeno,up_plati_do)
+		VALUES ('$user','$castka','$placeno','$plati_do')");
+	}
+	public static function editPlatba($id, $user, $castka, $placeno, $plati_do) {
+		list($id, $user, $castka, $placeno, $plati_do) =
+			DBPlatby::escapeArray(array($id, $user, $castka, $placeno, $plati_do));
+		
+		DBPlatby::query("UPDATE users_platby
+		SET up_id_user='$user',up_castka='$castka',up_placeno='$placeno',up_plati_do='$plati_do'
+		WHERE up_id='$id'");
+		return true;
+	}
+	public static function removePlatba($id) {
+		list($id) = DBPlatby::escapeArray(array($id));
+		
+		DBPlatby::query("DELETE FROM users_platby WHERE up_id='$id'");
+		return true;
+	}
+	public static function removePlatbyFromUser($id) {
+		list($id) = DBPlatby::escapeArray(array($id));
+		
+		DBPlatby::query("DELETE FROM users_platby WHERE up_id_user='$id'");
+		return true;
+	}
+}
+?>
