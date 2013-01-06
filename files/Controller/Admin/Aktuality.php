bd23e7f7527825ee265b09439096530c92ae3520
dradancer <kuba.zarybnicky@post.cz>
2013-01-06 12:39:23 +0100

    Begun transition to new version

---
+++ files/Controller/Admin/Aktuality.php
@@ -0,0 +1,111 @@
+<?php
+class Controller_Admin_Aktuality implements Controller_Interface {
+	function view($id = null) {
+		if(empty($_POST)) {
+			include("files/Admin/Aktuality/Display.inc");
+			return;
+		}
+		
+		switch(post("action")) {
+			case 'edit':
+				$aktuality = post('aktuality');
+				if($aktuality[0])
+					View::redirect('/admin/aktuality/edit/' . $aktuality[0]);
+				break;
+			case 'foto':
+				$aktuality = post('aktuality');
+				if($aktuality[0])
+					View::redirect('/admin/aktuality/foto/' . $aktuality[0]);
+				break;
+			case 'remove': //FIXME:URI Building
+				if(!is_array(post('akce')))
+					break;
+				$url = Request::getURI() . '/remove';
+				foreach(post('akce') as $id)
+					$url .= '&u[]=' . $id;
+				View::redirect($url);
+				break;
+		}
+		include("files/Admin/Aktuality/Display.inc");
+	}
+	function add($id = null) {
+		if(empty($_POST)) {
+			include('files/Admin/Aktuality/Form.inc');
+			return;
+		}
+		$preview = trim(substr(strip_tags(post('text')), 0, AKTUALITY_PREVIEW));
+		
+		if(($foto_data = DBGalerie::getFotky(post('foto'))) &&
+				$foto_data[0]['gf_id'])
+			$f_id = $foto_data[0]['gf_id'];
+		else
+			$f_id = 0;
+		
+		DBAktuality::addAktualita(User::getUserID(), post('kat'), post('jmeno'),
+			post('text'), $preview, post('foto'), $f_id);
+		DBNovinky::addNovinka('Uživatel ' . User::getUserWholeName() . ' přidal článek ' .
+			post('jmeno'));
+		View::redirect('/admin/aktuality', 'Článek přidán');
+	}
+	function edit($id = null) {
+		if(!$id || !($data = DBAktuality::getSingleAktualita($id)))
+			View::redirect('/admin/aktuality', 'Článek s takovým ID neexistuje');
+		
+		if(!Permissions::canEditAktualita($data['at_kdo']))
+			View::viewError(ER_AUTHORIZATION);
+		
+		if(empty($_POST)) {
+			post('kat', $data['at_kat']);
+			post("jmeno", $data["at_jmeno"]);
+			post("text", stripslashes($data["at_text"]));
+			post('foto', $data['at_foto']);
+			
+			include("files/Admin/Aktuality/Form.inc");
+			return;
+		}
+		if(post('kat') != $data['at_kat'] || post('jmeno') != $data['at_jmeno'] ||
+				post('text') != $data['at_text'] || post('foto') != $data['at_foto']) {
+			$preview = trim(substr(strip_tags(post('text')), 0, AKTUALITY_PREVIEW));
+			
+			if(($foto_data = DBGalerie::getFotky(post('foto'))) &&
+					$foto_data[0]['gf_id'])
+				$f_id = $foto_data[0]['gf_id'];
+			else
+				$f_id = 0;
+			
+			DBAktuality::editAktualita($id, post('kat'), post('jmeno'), post('text'),
+				$preview, post('foto'), $f_id);
+			$changed = true;
+		}
+		if(isset($changed) && $changed) {
+			DBNovinky::addNovinka('Uživatel ' . User::getUserWholeName() . ' upravil článek ' .
+				post('jmeno'));
+		}
+		
+		View::redirect('/admin/aktuality', 'Článek změněn');
+	}
+	function remove($id = null) {
+		if(empty($_POST) || post('action') !== 'confirm') {
+			include('files/Admin/Akce/DisplayRemove.inc');
+			return;
+		}
+		if(!is_array(post('aktuality')))
+			View::redirect('/admin/aktuality');
+		foreach(post('aktuality') as $id) {
+			$data = DBAktuality::getSingleAktualita($id);
+			
+			if(Permissions::canEditAktualita($data['at_kdo'])) {
+				DBAktuality::removeAktualita($id);
+				DBNovinky::addNovinka('Uživatel ' . User::getUserWholeName() . ' vymazal článek ' .
+					$data['at_jmeno']);
+			} else {
+				$error = true;
+			}
+		}
+		if(isset($error) && $error)
+			View::viewError(ER_AUTHORIZATION);
+		
+		View::redirect('/admin/aktuality', 'Články odebrány');
+    }
+}
+?>
---
+++ files/Controller/Admin/Rozpis/Detail.php
@@ -0,0 +1,97 @@
+<?php
+class Controller_Admin_Rozpis_Detail implements Controller_Interface {
+	function view($id) {
+		if(!$id || !($data = DBRozpis::getSingleRozpis($id)))
+			View::redirect('/admin/rozpis', 'Rozpis s takovým ID neexistuje');
+		if(!Permissions::canEditRozpis($data['r_trener']))
+			View::viewError(ER_AUTHORIZATION);
+		
+		$items = DBRozpis::getRozpisItem($id);
+		$users = DBPary::getPartners();
+		
+		if(empty($_POST)) {
+			include("files/Admin/RozpisDetail/Display.inc");
+			return;
+		}
+		if(post("generate") == "overlap") {
+			$end = "00:00";
+			foreach($items as $item) {
+				$l_start = formatTime($item["ri_od"], 1);
+				$l_end = formatTime($item["ri_do"], 1);	
+				$length = timeSubstract($l_end, $l_start);
+				
+				if(strcmp($end, $l_start) > 0) {
+					$l_start = $end;
+					$l_end = timeAdd($l_start, $length);
+				}
+				if(strcmp($l_start, $l_end) > 0)
+					$l_end = timeAdd($l_start, $length);
+				
+				DBRozpis::editRozpisItem($item["ri_id"], $item["ri_partner"],
+					formatTime($l_start, 0), formatTime($l_end, 0), $item["ri_lock"]);
+				
+				$end = $l_end;
+			}
+			$items = DBRozpis::getRozpisItem($id);
+		} elseif(post("generate") == "gen_add" &&
+				is_numeric(post("gen_add_hod")) &&
+				is_numeric(post("gen_add_len")) &&
+				preg_match("/^[0-9]{2}:[0-9]{2}$/", post("gen_add_od"))) {
+			
+			$od = post("gen_add_od");
+			$len = floor(post("gen_add_len") / 60) . ":" .
+				(int)(post("gen_add_len") % 60);
+			$do = timeAdd($od, $len);
+			
+			for($i = 0; $i < post("gen_add_hod"); $i++) {
+				DBRozpis::addRozpisItem($id, "0", formatTime($od, 0),
+					formatTime($do, 0), "0");
+				$od = $do;
+				$do = timeAdd($od, $len);
+			}
+			$items = DBRozpis::getRozpisItem($id);
+		} else {
+			if(post("remove") > 0) {
+				DBRozpis::removeRozpisItem(post("remove"));
+				$items = DBRozpis::getRozpisItem($id);
+			}
+			foreach($items as $item) {
+				if(!(post($item["ri_id"] . "-partner") == $item["ri_partner"] ||
+						(post($item["ri_id"] . "-partner") == "none" && $item["ri_partner"] =="0")) ||
+						post($item["ri_id"] . "-od") != formatTime($item["ri_od"], 1) ||
+						post($item["ri_id"] . "-do") != formatTime($item["ri_do"], 1) ||
+						(bool) post($item["ri_id"] . "-lock") != (bool) $item["ri_lock"]) {
+					
+					$partner = (post($item["ri_id"] . "-partner") == "none") ? "0" :
+						post($item["ri_id"] . "-partner");
+					$od = post($item["ri_id"] . "-od");
+					$do = post($item["ri_id"] . "-do");
+					$lock = post($item["ri_id"] . "-lock") ? 1 : 0;
+					
+					if(preg_match("/^[0-9]{2}:[0-9]{2}$/", $od) &&
+							preg_match("/^[0-9]{2}:[0-9]{2}$/", $do)) {
+						DBRozpis::editRozpisItem($item["ri_id"], $partner,
+							formatTime($od, 0), formatTime($do, 0), $lock);
+					}
+				}
+			}
+			$items = DBRozpis::getRozpisItem($id);
+			
+			if(is_numeric(post("add_partner")) || post("add_partner") == "none") {
+				$partner = (post("add_partner") == "none") ? "0" : post("add_partner");
+				$od = post("add_od");
+				$do = post("add_do");
+				$lock = post("add_lock");
+				
+				if(preg_match("/^[012]*[0-9]{1}:[0-9]{2}$/", $od) &&
+						preg_match("/^[012]*[0-9]{1}:[0-9]{2}$/", $do)) {
+					DBRozpis::addRozpisItem($id, $partner, formatTime($od, 0), formatTime($do, 0), $lock);
+					post('add_partner', null);
+					$items = DBRozpis::getRozpisItem($id);
+				}
+			}
+		}
+		include("files/Admin/RozpisDetail/Display.inc");
+	}
+}
+?>
