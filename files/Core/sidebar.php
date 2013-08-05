dc3f82694b4ab59d145bba408e1361544bc6c265
dradancer <kuba.zarybnicky@post.cz>
2013-08-05 21:51:32 +0200

    Repaired menu-button's clickable area

--- files/Core/sidebar.php
+++ files/Core/sidebar.php
@@ -8,19 +8,27 @@ class Sidebar {
 			return;
 		
 		$active = stripos(Request::getURI(), $link) === 0;
-		$r = '<a href="' . $link . '"><span class="arrow">.</span>' . $text . '</a>';
+		$r = '<span class="arrow">.</span>' . $text;
 		if($active)
-			return $this->blackBox($r . '<span class="point">.</span>', 'sidebar current');
+			return $this->blackBox($r . '<span class="point">.</span>', 'sidebar current', $link);
 		else
-			return $this->whiteBox($r, 'sidebar');
+			return $this->whiteBox($r, 'sidebar', $link);
 	}
-	function blackBox($text, $class = '') {
+	function blackBox($text, $class = '', $link = '') {
+		$text = '<div class="dark-in">' . $text . '</div>';
+		if($link != '') {
+			$text = '<a href="' . $link . '">' . $text . '</a>';
+		}
 		return '<div class="dark-out' .
-			($class ? ' ' . $class : '') . '"><div class="dark-in">' . $text . '</div></div>';
+			($class ? ' ' . $class : '') . '">' . $text . '</div>';
 	}
-	function whiteBox($text, $class = '') {
+	function whiteBox($text, $class = '', $link = '') {
+		$text = '<div class="light-in">' . $text . '</div>';
+		if($link != '') {
+			$text = '<a href="' . $link . '">' . $text . '</a>';
+		}
 		return '<div class="light-out' .
-			($class ? ' ' . $class : '') . '"><div class="light-in">' . $text . '</div></div>';
+			($class ? ' ' . $class : '') . '">' . $text . '</div>';
 	}
 }
 ?>
