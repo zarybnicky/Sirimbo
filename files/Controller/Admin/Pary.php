90e01d68d78b707a605e87edde43f0e5506460f6
dradancer <kuba.zarybnicky@post.cz>
2013-08-27 13:50:57 +0200

    Rewrote Admin_Pary,Permissions,Novinky,Nastenka to $this->render() format.
    Also tinkered a bit with Eclipse configuration and error reporting.
    
    Signed-off-by: dradancer <kuba.zarybnicky@post.cz>

--- files/Controller/Admin/Pary.php
+++ files/Controller/Admin/Pary.php
@@ -5,10 +5,6 @@ class Controller_Admin_Pary extends Controller_Admin {
 		Permissions::checkError('pary', P_OWNED);
 	}
 	function view($id = null) {
-		if(empty($_POST)) {
-			include("files/Admin/Pary/Display.inc");
-			return;
-		}
 		switch(post("action")) {
 			case "remove":
 				if(!is_array(post("pary")))
@@ -21,9 +17,8 @@ class Controller_Admin_Pary extends Controller_Admin {
 				if($data['gal_id'])
 					DBPary::noPartner($data['gal_id']);
 				
-				notice('Pár odstraněn');
+				$this->redirect()->setRedirectMessage('Pár odstraněn');
 				break;
-				
 			case 'add':
 				$old_gal = DBPary::getLatestPartner(post("add_partner"), 'm');
 				$old_guy = DBPary::getLatestPartner(post("add_partnerka"), 'f');
@@ -35,21 +30,36 @@ class Controller_Admin_Pary extends Controller_Admin {
 				if($old_gal['u_id'])
 					DBPary::noPartner($old_gal['u_id']);
 				
-				notice('Pár přidán');
+				$this->redirect()->setRedirectMessage('Pár přidán');
 				break;
-			
 			case 'edit':
 				$pary = post('pary');
 				if($pary[0])
-					View::redirect('/admin/pary/edit/' . $pary[0]);
+					$this->redirect('/admin/pary/edit/' . $pary[0]);
 				break;
 		}
-		
-		include("files/Admin/Pary/Display.inc");
+		$data = DBPary::getActivePary();
+		foreach($data as &$row) {
+			$new_data = array(
+					'checkBox' => '<input type="checkbox" name="pary[]" value="' . $row["p_id"] . '" />',
+					'fullNameMan' => $row['guy_surname'] . ', ' . $row['guy_name'],
+					'fullNameWoman' => $row['gal'] ? ($row['gal_surname'] . ', ' . $row['gal_name']) : '',
+					'standart' => $row['p_stt_trida'] . ' ' . $row['p_stt_body'] . 'F' . $row['p_stt_finale'],
+					'latina' => $row['p_lat_trida'] . ' ' . $row['p_lat_body'] . 'F' . $row['p_lat_finale'],
+					'hodnoceni' => $row['p_hodnoceni']
+			);
+			$row = $new_data;
+		}
+		$this->render("files/View/Admin/Pary/Overview.inc", array(
+				'showMenu' => !TISK,
+				'data' => $data,
+				'usersMen' => DBUser::getUsersByPohlavi('m'),
+				'usersWomen' => DBUser::getUsersByPohlavi('f')
+		));
 	}
 	function edit($id = null) {
 		if(!$id || !($data = DBPary::getSinglePar($id)))
-			View::redirect('/admin/pary', 'Pár s takovým ID neexistuje');
+			$this->redirect('/admin/pary', 'Pár s takovým ID neexistuje');
 
 		if(empty($_POST)) {
 			post('stt-trida', $data['p_stt_trida']);
@@ -59,7 +69,10 @@ class Controller_Admin_Pary extends Controller_Admin {
 			post('lat-body', $data['p_lat_body']);
 			post('lat-finale', $data['p_lat_finale']);
 			
-			include('files/Admin/Pary/Form.inc');
+			$this->render('files/View/Admin/Pary/Form.inc', array(
+					'fullName' => $data['guy_name'] . ' ' . $data['guy_surname'] . ' - ' .
+						$data['gal_name'] . ' ' . $data['gal_surname']
+			));
 			return;
 		}
 		$stt_body = (post('stt-body') && is_numeric(post('stt-body'))) ?
@@ -84,7 +97,7 @@ class Controller_Admin_Pary extends Controller_Admin {
 		DBPary::editTridaBody($data['p_id'], post('stt-trida'), $stt_body,
 			$stt_finale, post('lat-trida'), $lat_body, $lat_finale, $hodnoceni);
 		
-		View::redirect('/admin/pary', 'Třída a body změněny');
+		$this->redirect('/admin/pary', 'Třída a body změněny');
 	}
 }
 ?>
