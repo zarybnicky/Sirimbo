cfd0ea723d875736a00562124a4d07402a99ad8d
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2014-09-01 17:13:00 +0200

    Fix platby view

--- files/Core/database/dbskupiny.php
+++ files/Core/database/dbskupiny.php
@@ -47,7 +47,7 @@ class DBSkupiny extends Database
                     LEFT JOIN platby_group ON pgs_id_group=pg_id
                     LEFT JOIN skupiny ON pgs_id_skupina=s_id
                     LEFT JOIN platby_category ON pcg_id_category=pc_id
-                WHERE s_id='$id'
+                WHERE s_id='$id' AND pc_archive!='1'
                 ORDER BY pg_type,pg_id,pc_date_due ASC"
         );
         return self::getArray($res);
