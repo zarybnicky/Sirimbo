97b5064b473f0e06ea3d578d3378a18f2c0fb5c2
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2014-10-16 14:21:52 +0200

    Fix 'zaplaceno' query

--- files/Core/database/dbuser.php
+++ files/Core/database/dbuser.php
@@ -259,38 +259,31 @@ class DBUser extends Database implements Pagable
         WHERE u_pohlavi='$pohlavi' ORDER BY u_prijmeni");
         return DBUser::getArray($res);
     }
-
+            
     public static function getUsersWithSkupinaPlatby() {
         $res = self::query(
             "SELECT *
             FROM users
-                LEFT JOIN skupiny on s_id=u_skupina
-                LEFT JOIN platby_group_skupina ON pgs_id_skupina=s_id
-                LEFT JOIN platby_group ON pg_id=pgs_id_group
-                LEFT JOIN platby_category_group ON pcg_id_group=pg_id
-                LEFT JOIN platby_category
-                    ON (
-                        pcg_id_category=pc_id AND
-                        (pc_use_prefix='0' AND (CURDATE() >= pc_valid_from AND CURDATE() <= pc_valid_to)) OR
+                LEFT JOIN (
+                    SELECT * FROM platby_item
+                        JOIN platby_category ON pc_id=pi_id_category
+                    WHERE
+                        (pc_use_prefix='0' AND
+                         CURDATE() >= pc_valid_from AND
+                         CURDATE() <= pc_valid_to)
+                        OR
                         (pc_use_prefix='1' AND
-                            DATE_SUB(CURDATE(), INTERVAL (YEAR(CURDATE())) YEAR) <= pc_valid_from AND
-                            DATE_SUB(CURDATE(), INTERVAL (YEAR(CURDATE())) YEAR) >= pc_valid_to)
-                    )
-                LEFT JOIN platby_item
-                    ON (
-                        u_id=pi_id_user AND
-                        pi_id_category=pc_id AND
-                        (
-                            (pc_use_prefix='1' AND pi_prefix = YEAR(CURDATE())) OR
-                            pc_use_prefix='0'
-                        )
-                    )
+                         DATE_SUB(CURDATE(), INTERVAL (YEAR(CURDATE())) YEAR) <= pc_valid_from AND
+                         DATE_SUB(CURDATE(), INTERVAL (YEAR(CURDATE())) YEAR) >= pc_valid_to)
+                ) pi ON u_id=pi_id_user
+                JOIN skupiny on s_id=u_skupina
+                LEFT JOIN platby_group_skupina ON pgs_id_skupina=s_id
+                LEFT JOIN platby_category_group ON pcg_id_category=pc_id
+                LEFT JOIN platby_group ON pg_id=pgs_id_group AND ((pg_id=pcg_id_group AND pi_id IS NOT NULL) OR (pi_id IS NULL))
             WHERE
-                u_confirmed='1' AND u_ban='0' AND u_system='0' AND
-                (pg_type='1' OR pg_type IS NULL) AND pc_id IS NOT NULL
+                u_confirmed='1' AND u_ban='0' AND u_system='0' AND ((pg_type='1' AND pi_id IS NOT NULL) OR (pi_id IS NULL AND (pg_type='1' OR pg_type IS NULL)))
             GROUP BY u_id
-            ORDER BY s_id,u_prijmeni
-        "
+            ORDER BY s_id,u_prijmeni"
         );
         return self::getArray($res);
     }
