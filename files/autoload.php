58e6ff4175390f8e15563cd530e884725c08a686
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2014-11-09 17:53:42 +0100

    Add autoload.php

---
+++ files/autoload.php
@@ -0,0 +1,13 @@
+<?php
+$core = __DIR__ . DIRECTORY_SEPARATOR . 'Core' . DIRECTORY_SEPARATOR;
+set_include_path(
+    $core . PATH_SEPARATOR .
+    $core . 'database' . PATH_SEPARATOR .
+    $core . 'display' . PATH_SEPARATOR .
+    $core . 'helpers' . PATH_SEPARATOR .
+    $core . 'paging' . PATH_SEPARATOR .
+    $core . 'types' . PATH_SEPARATOR .
+    get_include_path()
+);
+spl_autoload_extensions('.php');
+spl_autoload_register();
