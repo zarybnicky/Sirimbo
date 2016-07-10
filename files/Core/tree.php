351647b19f5407844343f5b77d33ef96003743dc
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2016-07-10 14:22:45 +0200

    Add Widget from phpCore

---
+++ files/Core/tree.php
@@ -0,0 +1,117 @@
+<?php
+class Tree
+{
+    protected $name;
+    protected $value;
+    protected $children;
+
+    public function __construct($name, $value, $children = array())
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->children = $children;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    public function find($name)
+    {
+        return isset($this->children[$name]) ? $this->children[$name] : false;
+    }
+
+    public function add(Tree $element)
+    {
+        $this->children[] = $element;
+    }
+
+    public function remove(Tree $element)
+    {
+        $key = array_search($this->children, $element);
+        if ($key) {
+            unset($this->children[$key]);
+        }
+    }
+
+    public function addAt($n, Tree $element)
+    {
+        array_splice($this->children, $n, 0, array($element));
+    }
+
+    public function removeAt($n)
+    {
+        array_splice($this->children, $n, 1);
+    }
+
+    public function update($fn)
+    {
+        $this->value = $fn($this->value, $this);
+    }
+
+    public function traverse($fn)
+    {
+        $stack = array($this);
+        while ($stack) {
+            $current = array_pop($stack);
+            if ($current instanceof Tree) {
+                $current->update($fn);
+                if ($children = $current->getChildren()) {
+                    for (end($children); key($children) !== null; prev($children)) {
+                        $stack[] = current($children);
+                    }
+                }
+            }
+        }
+    }
+
+    public function flattenPostorder()
+    {
+        $result = array();
+        $stack = array($this);
+        $ancestors = array();
+        while ($stack) {
+            $x = end($stack);
+            if ($children = $x->getChildren()) {
+                if ($x !== end($ancestors)) {
+                    $ancestors[] = $x;
+                    for (end($children); key($children) !== null; prev($children)) {
+                        $stack[] = current($children);
+                    }
+                    continue;
+                }
+                array_pop($ancestors);
+            }
+            $result[] = $x->getValue();
+            array_pop($stack);
+        }
+        return $result;
+    }
+
+    public function __toString()
+    {
+        $children = '';
+        foreach ($this->getChildren() as $child) {
+            $children .= "\n" . implode(
+                "\n", array_map(
+                    function ($x) {
+                        return '  ' . $x;
+                    },
+                    explode("\n", (string) $child)
+                )
+            );
+        }
+        return "{$this->name} (" . gettype($this->value) . ")$children";
+    }
+}
---
+++ files/Core/widget.php
@@ -0,0 +1,114 @@
+<?php
+class Widget extends Tree
+{
+    protected $stylesheets = array();
+    protected $scripts = array();
+
+    public function __construct(array $attributes = array(), $child = array())
+    {
+        $children = array();
+        array_walk_recursive(
+            (array_slice(func_get_args(), 1)),
+            function ($a) use (&$children) {
+                $children[] = $a;
+            }
+        );
+        parent::__construct(
+            get_called_class(),
+            $attributes,
+            $children
+        );
+    }
+
+    public function get($name = null)
+    {
+        if ($name === null) {
+            return $this->value;
+        }
+        return isset($this->value[$name]) ? $this->value[$name] : null;
+    }
+
+    public function set($key, $value)
+    {
+        $this->value[$key] = $value;
+    }
+
+    public function setAttributes(array $new)
+    {
+        $this->value = array_merge($this->value, $new);
+    }
+
+    public function getStylesheets()
+    {
+        return $this->stylesheets;
+    }
+
+    public function getScripts()
+    {
+        return $this->scripts;
+    }
+
+    public function addStylesheet(array $attributes, $source = '')
+    {
+        $attributes = array_merge(array('rel' => 'stylesheet'), $attributes);
+        $this->stylesheets[] = new Tag('style', $attributes, $source);
+    }
+
+    public function addScript(array $attributes, $source = '')
+    {
+        $this->scripts[] = new Tag('script', $attributes, $source);
+    }
+
+    public function render()
+    {
+        return '';
+    }
+
+    public function renderToString()
+    {
+        $result = array($this->render());
+        $pre = array();
+        $post = array();
+        $this->traverse(
+            function ($val, $x) use (&$pre, &$post) {
+                if ($x instanceof self) {
+                    $pre[] = $x->getStylesheets();
+                    $post[] = $x->getScripts();
+                }
+                return $val;
+            }
+        );
+        return $this->renderChild(array_merge($pre, $result, $post));
+    }
+
+    public function __toString()
+    {
+        return $this->renderToString();
+    }
+
+    protected function renderChild($element)
+    {
+        $result = '';
+        $stack = array($element);
+        while ($stack) {
+            $x = array_pop($stack);
+            while ($x instanceof self) {
+                $x = $x->render();
+            }
+            if (is_string($x)) {
+                $result .= $x;
+                continue;
+            }
+            if (is_array($x)) {
+                if ($x) {
+                    for (end($x); key($x) !== null; prev($x)) {
+                        $stack[] = current($x);
+                    }
+                }
+                continue;
+            }
+            trigger_error('Invalid Widget child (' . gettype($x) . '), ignoring.');
+        }
+        return $result;
+    }
+}
