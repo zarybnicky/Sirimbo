f2b460e98502c3063414108702efd50a552e007a
Jakub Zárybnický <jakub.zarybnicky@inuits.eu>
2018-09-30 22:06:21 +0200

    Upgrade admin/platby, substitute ŧ -> t

--- files/Controller/Admin/Platby/Items.php
+++ files/Controller/Admin/Platby/Items.php
@@ -9,24 +9,34 @@ class Controller_Admin_Platby_Items extends Controller_Admin_Platby
 
     public function view($request)
     {
-        switch ($request->post('action')) {
-            case 'edit':
-                $users = $request->post('data');
-                if ($users[0]) {
-                    $this->redirect('/admin/platby/items/edit/' . $users[0]);
-                }
-                break;
-
-            case 'remove':
-                if (is_array($request->post('data'))) {
-                    $this->redirect(
-                        '/admin/platby/items/remove?'
-                        . http_build_query(['u' => $request->post('data')])
-                    );
-                }
-                break;
+        $filter = [];
+        if ($request->get('user') && is_numeric($request->get('user'))) {
+            $filter['u_id'] = $request->get('user');
         }
-        $data = $this->getData($request);
+        if ($request->get('category') && is_numeric($request->get('category'))) {
+            $filter['pc_id'] = $request->get('category');
+        } elseif (stripos($request->get('category'), 'group_') !== false) {
+            $filter['pg_id'] = substr($request->get('category'), 6);
+        }
+        $date = $this->date('date')->range()->getPostRange($request);
+
+        $data = DBPlatbyItem::get(true, $filter, ['pi_date DESC'], $date);
+
+        $data = array_map(
+            function ($item) {
+                return [
+                    'buttons' => $this->editLink('/admin/platby/items/edit/' . $item['pi_id'])
+                        . '&nbsp;&nbsp;'
+                        . $this->removeLink('/admin/platby/items/remove/' . $item['pi_id']),
+                    'fullName' => $item['u_prijmeni'] . ', ' . $item['u_jmeno'],
+                    'category' => $item['pc_name'],
+                    'date' => (new Date($item['pi_date']))->getDate(Date::FORMAT_SIMPLE_SPACED),
+                    'amount' => $item['pi_amount'] . ' Kč'
+                ];
+            },
+            $data
+        );
+
         $this->render(
             'files/View/Admin/Platby/ItemsOverview.inc',
             [
@@ -40,6 +50,7 @@ class Controller_Admin_Platby_Items extends Controller_Admin_Platby
             ]
         );
     }
+
     public function add($request)
     {
         if (!$request->post()) {
@@ -60,6 +71,7 @@ class Controller_Admin_Platby_Items extends Controller_Admin_Platby
         );
         $this->redirect('/admin/platby/items', 'Platba úspěšně přidána');
     }
+
     public function edit($request)
     {
         $id = $request->getId();
@@ -92,43 +104,37 @@ class Controller_Admin_Platby_Items extends Controller_Admin_Platby
 
     public function remove($request)
     {
-        if (!is_array($request->post('data')) && !is_array($request->get('u'))) {
+        if (!$request->getId()) {
             $this->redirect('/admin/platby/items');
         }
-        if ($request->post() && $request->post('action') == 'confirm') {
-            foreach ($request->post('data') as $id) {
-                $item = DBPlatbyItem::getSingle($id);
-                $itemRaw = DBPlatbyRaw::getSingle($item['pi_id_raw']);
-
-                DBPlatbyItem::remove($id);
-                if ($item['pi_id_raw']) {
-                    DBPlatbyRaw::update(
-                        $item['pi_id_raw'],
-                        $itemRaw['pr_raw'],
-                        $itemRaw['pr_hash'],
-                        '0',
-                        '1'
-                    );
-                }
+        $id = $request->getId();
+        if ($request->post('action') == 'confirm') {
+            $item = DBPlatbyItem::getSingle($id);
+            $itemRaw = DBPlatbyRaw::getSingle($item['pi_id_raw']);
+            DBPlatbyItem::remove($id);
+            if ($item['pi_id_raw']) {
+                DBPlatbyRaw::update(
+                    $item['pi_id_raw'],
+                    $itemRaw['pr_raw'],
+                    $itemRaw['pr_hash'],
+                    '0',
+                    '1'
+                );
             }
             $this->redirect('/admin/platby/items', 'Platby odebrány');
         }
-        $data = [];
-        foreach ($request->get('u') as $id) {
-            $item = DBPlatbyItem::getSingle($id, true);
-            $data[] = [
-                'id' => $item['pi_id'],
-                'text' => $item['u_jmeno'] . ' ' . $item['u_prijmeni']
-                    . ' - ' . $item['pc_name']
-            ];
-        }
+        $item = DBPlatbyItem::getSingle($id, true);
         $this->render(
             'files/View/Admin/RemovePrompt.inc',
             [
                 'header' => 'Správa plateb',
-                'prompt' => 'Opravdu chcete odstranit platby:',
+                'prompt' => 'Opravdu chcete odstranit platbu:',
                 'returnURI' => $request->getReferer() ?: '/admin/platby/items',
-                'data' => $data
+                'data' => [[
+                    'id' => $item['pi_id'],
+                    'text' => $item['u_jmeno'] . ' ' . $item['u_prijmeni']
+                    . ' - ' . $item['pc_name']
+                ]]
             ]
         );
     }
@@ -186,34 +192,5 @@ class Controller_Admin_Platby_Items extends Controller_Admin_Platby
             $array = User::varSymbol($array['u_id']) . " - {$array['u_prijmeni']}, {$array['u_jmeno']}";
         }
         return $users;
-    }
-
-    private function getData($request)
-    {
-        $filter = [];
-        if ($request->get('user') && is_numeric($request->get('user'))) {
-            $filter['u_id'] = $request->get('user');
-        }
-        if ($request->get('category') && is_numeric($request->get('category'))) {
-            $filter['pc_id'] = $request->get('category');
-        } elseif (stripos($request->get('category'), 'group_') !== false) {
-            $filter['pg_id'] = substr($request->get('category'), 6);
-        }
-        $date = $this->date('date')->range()->getPostRange($request);
-
-        $data = DBPlatbyItem::get(true, $filter, ['pi_date DESC'], $date);
-
-        return array_map(
-            function ($item) {
-                return [
-                    'checkBox' => $this->checkbox('data[]', $item['pi_id'])->render(),
-                    'fullName' => $item['u_prijmeni'] . ', ' . $item['u_jmeno'],
-                    'category' => $item['pc_name'],
-                    'date' => (new Date($item['pi_date']))->getDate(Date::FORMAT_SIMPLE_SPACED),
-                    'amount' => $item['pi_amount'] . 'Kč'
-                ];
-            },
-            $data
-        );
     }
 }
