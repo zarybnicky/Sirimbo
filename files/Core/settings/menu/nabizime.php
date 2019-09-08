5341a2f0a7908cf6e4e15d8fe0b7f6a837453a24
Jakub Zárybnický <kubaz@inuits.eu>
2019-09-09 01:00:24 +0200

    Prune /nabizime

--- files/Core/settings/menu/nabizime.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    ['Taneční kurzy', '/nabizime/obecne', []],
-    ['Taneční vystoupení', '/nabizime/vystoupeni', []],
-    ['Taneční soustředění', '/nabizime/soustredeni', []],
-    ['Tancování pro děti', '/nabizime/seminare', []],
-    ['Zázemí pro taneční sport', '/nabizime/individualky', []],
-];
