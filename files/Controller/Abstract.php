ff53c90d6fca9021ee94ad193624b8c8f647d371
Jakub Zárybnický <kubaz@inuits.eu>
2019-09-09 03:51:46 +0200

    Clean up Controller_Abstract

--- files/Controller/Abstract.php
+++ files/Controller/Abstract.php
@@ -3,31 +3,6 @@ abstract class Controller_Abstract implements Controller_Interface
 {
     abstract public function view($request);
 
-    public function renderNavbarItem($item)
-    {
-        if (!is_array($item)) {
-            return "<div class='$item'></div>";
-        }
-        $active = $item[1] === ('/' . Database::$request->getURI())
-            || (strlen($item[1]) > 1 && strpos('/' . Database::$request->getURI(), $item[1]) === 0);
-        $active = $active ? ' active' : '';
-
-        if (!isset($item[2]) || empty($item[2])) {
-            return '<li class="nav-item' . $active . '"><a class="nav-link" href="'
-                . $item[1] . '">' . $item[0] . '</a></li>';
-        }
-        $x = '<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">' . $item[0] . '</a>';
-        $x .= '<div class="dropdown-menu">';
-        foreach ($item[2] as $sub) {
-            if (isset($sub[3]) && $sub[3] && !Permissions::check($sub[3][0], $sub[3][1])) {
-                continue;
-            }
-            $x .= '<a class="dropdown-item" href="' . $sub[1] . '">' . $sub[0] . '</a>';
-        }
-        $x .= '</div>';
-        return '<li class="nav-item' . $active . ' dropdown">' . $x . '</li>';
-    }
-
     public function render($filename, array $vars = [], $standalone = false)
     {
         $renderer = new Renderer();
@@ -38,8 +13,7 @@ abstract class Controller_Abstract implements Controller_Interface
             return;
         }
 
-        $args = [
-            'renderNavbarItem' => [$this, 'renderNavbarItem'],
+        echo $renderer->render(TEMPLATE, [
             'navbar' => [[
                 ['Klub', '/', [
                     ['Kluboví trenéři', '/oklubu/treneri/klubovi'],
@@ -73,28 +47,15 @@ abstract class Controller_Abstract implements Controller_Interface
                     ['Video', '/admin/video', [], ['aktuality', P_OWNED]],
                     ['Dokumenty', '/admin/dokumenty', [], ['dokumenty', P_OWNED]],
                     ['Oprávnění', '/admin/permissions', [], ['permissions', P_OWNED]],
+                    ['Stránky', '/admin/site', [], ['site', P_OWNED]],
                 ], ['nastenka', P_OWNED]]
             ]],
-            'meta' => [],
             'content' => $content,
+            'meta' => isset($vars['meta']) ? $vars['meta'] : [],
             'header' => isset($vars['header']) ? $vars['header'] : null,
             'subheader' => isset($vars['subheader']) ? $vars['subheader'] : null,
-            'html_title' => ''
-        ];
-        if (isset($vars['meta'])) {
-            $args['meta'] = array_map(
-                function ($k, $v) {
-                    return "$k=\"$v\"";
-                },
-                array_keys($vars['meta']),
-                $vars['meta']
-            );
-        }
-        if (isset($vars['html_title'])) {
-            $args['html_title'] = $vars['html_title'];
-        }
-
-        echo $renderer->render(TEMPLATE, $args);
+            'html_title' => isset($vars['html_title']) ? $vars['html_title'] : ''
+        ]);
     }
 
     public function __call($name, $args)
---
+++ files/Core/helpers/navbaritemhelper.php
@@ -0,0 +1,43 @@
+<?php
+class NavbarItemHelper
+{
+    protected $text;
+
+    public function navbarItem($item)
+    {
+        $this->item = $item;
+        return $this;
+    }
+
+    public function render()
+    {
+        $item = $this->item;
+
+        if (!is_array($item)) {
+            return "<div class='$item'></div>";
+        }
+        $active = $item[1] === ('/' . Database::$request->getURI())
+            || (strlen($item[1]) > 1 && strpos('/' . Database::$request->getURI(), $item[1]) === 0);
+        $active = $active ? ' active' : '';
+
+        if (!isset($item[2]) || empty($item[2])) {
+            return '<li class="nav-item' . $active . '"><a class="nav-link" href="'
+                . $item[1] . '">' . $item[0] . '</a></li>';
+        }
+        $x = '<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">' . $item[0] . '</a>';
+        $x .= '<div class="dropdown-menu">';
+        foreach ($item[2] as $sub) {
+            if (isset($sub[3]) && $sub[3] && !Permissions::check($sub[3][0], $sub[3][1])) {
+                continue;
+            }
+            $x .= '<a class="dropdown-item" href="' . $sub[1] . '">' . $sub[0] . '</a>';
+        }
+        $x .= '</div>';
+        return '<li class="nav-item' . $active . ' dropdown">' . $x . '</li>';
+    }
+
+    public function __toString()
+    {
+        return $this->render();
+    }
+}
