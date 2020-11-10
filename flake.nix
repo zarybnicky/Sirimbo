b41bf193f64217db5eedfff9c9c633654418d5a9
Jakub Zárybnický <kubaz@inuits.eu>
2020-11-10 13:16:55 +0100

    Add nixosModule

--- flake.nix
+++ flake.nix
@@ -54,5 +54,97 @@
         pkgs.php73Packages.phpstan
       ];
     };
+
+    nixosModules = {
+      olymp-api = { config, lib, pkgs, ... }: let
+        cfg = config.services.olymp-api;
+        pkgName = "api.tkolymp.cz";
+      in {
+        options.services.olymp-api = {
+          enable = lib.mkEnableOption "${pkgName}";
+          user = lib.mkOption {
+            type = lib.types.str;
+            default = "olymp";
+            description = "${pkgName} user";
+          };
+          group = lib.mkOption {
+            type = lib.types.str;
+            default = "olymp";
+            description = "${pkgName} group";
+          };
+          domain = lib.mkOption {
+            type = lib.types.str;
+            description = "${pkgName} Nginx vhost domain";
+            example = "api.tkolymp.cz";
+          };
+          port = lib.mkOption {
+            type = lib.types.int;
+            description = "${pkgName} internal port";
+            example = 3000;
+          };
+          dbHost = lib.mkOption {
+            type = lib.types.string;
+            description = "${pkgName} DB host";
+          };
+          dbUser = lib.mkOption {
+            type = lib.types.string;
+            description = "${pkgName} DB user";
+          };
+          dbPassword = lib.mkOption {
+            type = lib.types.string;
+            description = "${pkgName} DB password";
+          };
+          dbDatabase = lib.mkOption {
+            type = lib.types.string;
+            description = "${pkgName} DB database";
+          };
+        };
+
+        config = lib.mkIf cfg.enable {
+          users.users.${cfg.user} = {
+            name = cfg.user;
+            group = cfg.group;
+            description = pkgName;
+            isSystemUser = true;
+          };
+          users.groups.${cfg.user} = {
+            name = cfg.group;
+          };
+
+          services.nginx = {
+            enable = true;
+            enableReload = true;
+            recommendedGzipSettings = true;
+            recommendedOptimisation = true;
+            recommendedProxySettings = true;
+            virtualHosts.${cfg.domain}.locations."/" = {
+              proxyPass = "http://localhost:${toString cfg.port}";
+              proxyWebsockets = true;
+            };
+          };
+
+          systemd.services.olymp-api = {
+            description = "${pkgName} Webserver";
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network-online.target" ];
+            environment = {
+              CONFIG = pkgs.writeText "config.yaml" ''
+                dbHost: ${cfg.dbHost}
+                dbUser: ${cfg.dbUser}
+                dbPassword: ${cfg.dbPassword}
+                dbDatabase: ${cfg.dbDatabase}
+              '';
+            };
+            serviceConfig = {
+              User = cfg.user;
+              Group = cfg.group;
+              Restart = "always";
+              ExecStart = "${self.sirimbo-api}/bin/server";
+            };
+          };
+        };
+      };
+
+    };
   };
 }
