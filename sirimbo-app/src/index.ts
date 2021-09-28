159e5a8f12a032ba031a894046fa26a33af3e230
Jakub Zárybnický <kubaz@inuits.eu>
2021-09-28 19:16:34 +0200

    Fix assets

--- sirimbo-app/src/index.ts
+++ sirimbo-app/src/index.ts
@@ -2,7 +2,6 @@ import 'bootstrap';
 import './bundles/leaflet';
 import './bundles/select2.full';
 import './bundles/spectrum';
-import './bundles/stickyfill.min';
 import './bundles/video.min';
 import './bundles/main';
 
