3bbb462e0689a9f9f3d04799f6501bbd3d4c65fe
Jakub Zárybnický <kubaz@inuits.eu>
2021-10-04 12:52:50 +0200

    More date-range elements

--- sirimbo-app/src/date.tsx
+++ sirimbo-app/src/date.tsx
@@ -3,27 +3,46 @@ import * as ReactDOM from 'react-dom';
 import { format } from 'date-fns';
 
 interface DateRangeProps {
-    year?: string;
+    noYear?: string;
     from: string;
     to: string;
 }
-
-export function DateRange({ year, from, to }: DateRangeProps) {
-    const f = year !== undefined ? 'd.M.y' : 'd.M.';
+export function DateRange({ noYear, from, to }: DateRangeProps) {
+    const f = noYear !== undefined ? 'd. M.' : 'd. M. y';
     return <span>{
         (to && from != to)
             ? format(new Date(from), f) + ' - ' + format(new Date(to), f)
             : format(new Date(from), f)
     }</span>;
 }
-
 class DateRangeElement extends HTMLElement {
     connectedCallback() {
         ReactDOM.render(<DateRange
-            year={this.getAttribute('year') || undefined}
+            noYear={this.getAttribute('noYear') || undefined}
             from={this.getAttribute('from') || ''}
             to={this.getAttribute('to') || ''}
         />, this);
     }
 }
 customElements.define('date-range', DateRangeElement);
+
+interface DateProps {
+    noYear?: string;
+    timestamp?: string;
+    date: string;
+}
+export function DateEl({ noYear, timestamp, date }: DateProps) {
+    const f = timestamp !== undefined ? 'd. M. y H:mm' :
+        noYear !== undefined ? 'd. M.' : 'd. M. y';
+    return <span>{format(new Date(date), f)}</span>;
+}
+class DateElement extends HTMLElement {
+    connectedCallback() {
+        ReactDOM.render(<DateEl
+            noYear={this.getAttribute('noYear') || undefined}
+            timestamp={this.getAttribute('timestamp') || undefined}
+            date={this.getAttribute('date') || ''}
+        />, this);
+    }
+}
+customElements.define('date', DateElement);
