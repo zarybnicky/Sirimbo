3c343438b120ccd02a5466d09a2dccee1307fa7a
Jakub Zárybnický <kubaz@inuits.eu>
2021-10-26 12:31:54 +0200

    Fix aktuality

--- sirimbo-php/files/Controller/Aktualne.php
+++ sirimbo-php/files/Controller/Aktualne.php
@@ -8,7 +8,7 @@ class Aktualne
         \Render::twig('Main/Aktuality.twig', [
             'data' => array_for(\DBAktuality::getAktuality(), fn($item) => $item + [
                 'canEdit' => \Permissions::check('aktuality', P_OWNED, $item['at_kdo']),
-                'photo_uri' => \DBGalerie::getSingleFoto($item['at_foto_main'])['gf_path'] ?? '',
+                'photo_uri' => $item['at_foto_main'] ? (\DBGalerie::getSingleFoto($item['at_foto_main'])['gf_path'] ?? '') : '',
             ]),
         ]);
     }
@@ -20,7 +20,7 @@ class Aktualne
         }
         \Render::twig('Main/AktualitySingle.twig', [
             'data' => $data,
-            'photoUri' => \DBGalerie::getSingleFoto($data['at_foto_main'])['gf_path'] ?? '',
+            'photoUri' => $data['at_foto_main'] ? (\DBGalerie::getSingleFoto($data['at_foto_main'])['gf_path'] ?? '') : '',
             'canEdit' => \Permissions::check('aktuality', P_OWNED, $data['at_kdo']),
         ]);
     }
