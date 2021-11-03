b1f6d53f1509833605169d756f80a6793894bed6
Jakub Zárybnický <jakub@zarybnicky.com>
2021-11-03 10:28:06 +0100

    Delete unused persistent schemas

--- sirimbo-schema/Olymp/Schema.hs
+++ sirimbo-schema/Olymp/Schema.hs
@@ -1,24 +1,13 @@
 module Olymp.Schema
   ( PersistEntity(..)
   , EntityField(..)
-  , Unique(..)
   , Key(..)
   , module X
   ) where
 
 import Database.Persist
 
-import Olymp.Schema.Announcement as X
-import Olymp.Schema.Article as X
-import Olymp.Schema.Couple as X
-import Olymp.Schema.Document as X
-import Olymp.Schema.Event as X
-import Olymp.Schema.Payment as X
-import Olymp.Schema.PaymentGroup as X
 import Olymp.Schema.Permission as X
-import Olymp.Schema.Photo as X
-import Olymp.Schema.Reservation as X
 import Olymp.Schema.Runtime as X
-import Olymp.Schema.Schedule as X
 import Olymp.Schema.User as X
 import Olymp.Schema.Video as X
