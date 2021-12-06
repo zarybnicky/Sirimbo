c050f65094e12a2300ff1bfa08e3e5ea5615f5b8
Jakub Zárybnický <jakub@zarybnicky.com>
2021-12-06 14:48:35 +0100

    Finished ArticleList

--- sirimbo-app/src/components/ArticleList.tsx
+++ sirimbo-app/src/components/ArticleList.tsx
@@ -1,33 +1,82 @@
 import * as React from 'react';
-import { Container, Button, Typography, Card, CardActionArea, CardMedia, CardContent, CardActions } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import {
+  Container, Button, Grid, Typography, Card, CardMedia, CardContent,
+  CardActionArea, CardActions, makeStyles
+} from '@material-ui/core';
 
-export const ArticleList = () => {
+const useStyles = makeStyles((theme) => ({
+  header: {
+    color: theme.palette.primary.main,
+    fontWeight: 'bold',
+    position: 'relative',
+    marginBottom: '15px',
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      bottom: '-7px',
+      left: 0,
+      right: '50%',
+      height: '5px',
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+  buttons: {
+    justifyContent: 'center',
+    paddingBottom: '2px',
+  },
+}));
+
+export const ArticleList = ({ }) => {
+  const classes = useStyles();
   return <Container maxWidth="lg">
-    <Typography variant="h2">Aktuálně</Typography>
-    <Card>
-      <CardActionArea>
-        <CardMedia
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <Typography gutterBottom variant="h4" component="h2">Aktuálně</Typography>
+    <Grid container spacing={3}>
+      {items.map((x, i) => (
+        <Grid item sm={6} md={3}>
+          <Card key={i} elevation={3}>
+            <CardActionArea component={Link} to={x.href}>
+              <CardMedia component="img" height={240} image={x.img} title={x.header} />
+              <CardContent>
+                <Typography gutterBottom variant="subtitle1" component="h3" className={classes.header}>{x.header}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{x.preview}</Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.buttons}>
+              <Button size="large" variant="contained" color="primary" component={Link} to={x.href}>
+                Více zde ᐳ
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   </Container>;
 };
+
+const items = [
+  {
+    href: "/o-nas",
+    img: "https://picsum.photos/360/240?random=1",
+    header: "Přípravný kurz tanečního sportu",
+    preview: "Otevíráme kurz pro mládež a dospělé v Olomouci a Přerově"
+  },
+  {
+    href: "/o-nas",
+    img: "https://picsum.photos/360/240?random=2",
+    header: "Přípravný kurz tanečního sportu",
+    preview: "Otevíráme kurz pro mládež a dospělé v Olomouci a Přerově"
+  },
+  {
+    href: "/o-nas",
+    img: "https://picsum.photos/360/240?random=3",
+    header: "Přípravný kurz tanečního sportu",
+    preview: "Otevíráme kurz pro mládež a dospělé v Olomouci a Přerově"
+  },
+  {
+    href: "/o-nas",
+    img: "https://picsum.photos/360/240?random=4",
+    header: "Přípravný kurz tanečního sportu",
+    preview: "Otevíráme kurz pro mládež a dospělé v Olomouci a Přerově"
+  },
+];
