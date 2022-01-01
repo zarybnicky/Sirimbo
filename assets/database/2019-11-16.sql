fdde037339223ce82974ae05617a27e5bf735605
Jakub Zárybnický <jakub@zarybnicky.com>
2022-01-01 22:13:55 +0100

    Purge assets

--- assets/database/2019-11-16.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `session` (
-    `ss_id` VARCHAR(128) NOT NULL PRIMARY KEY,
-    `ss_data` BLOB NOT NULL,
-    `ss_updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `ss_lifetime` BIGINT NOT NULL
-);
