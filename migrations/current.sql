42940a565dc602fdd1a0fd32b963a0c5337b4fd0
Jakub Zárybnický <jakub@zarybnicky.com>
2022-02-02 13:49:49 +0100

    Add payment status to members view

--- migrations/current.sql
+++ migrations/current.sql
@@ -60,16 +60,18 @@ do $$ begin
 end $$;
 
 CREATE OR REPLACE VIEW public.members AS
-  SELECT DISTINCT ON (u_id, s_name, u_email) u_email, s_name FROM platby_item
-    INNER JOIN platby_category ON pc_id=pi_id_category
+  WITH current_payments as (
+    SELECT * from platby_item
+    INNER JOIN platby_category ON pi_id_category=pc_id
     INNER JOIN platby_category_group ON pcg_id_category=pc_id
     INNER JOIN platby_group ON pg_id=pcg_id_group
-    INNER JOIN platby_group_skupina ON pgs_id_group=pg_id
-    INNER JOIN skupiny ON pgs_id_skupina=s_id
-    INNER JOIN users ON pi_id_user=u_id AND u_skupina=s_id
-  WHERE pg_type='1'
+    WHERE pg_type='1'
     AND CURRENT_DATE >= pc_valid_from
     AND CURRENT_DATE <= pc_valid_to
-    AND u_confirmed='1'
+  )
+  SELECT u_email, s_name, (SELECT EXISTS (SELECT pi_id FROM current_payments WHERE pi_id_user=u_id)) as payment_valid
+    FROM users INNER JOIN skupiny ON s_id=u_skupina
+  WHERE u_confirmed='1'
     AND u_ban='0'
-    AND u_system='0' ORDER BY s_name, u_email;
+    AND u_system='0'
+  ORDER BY s_name, u_email;
