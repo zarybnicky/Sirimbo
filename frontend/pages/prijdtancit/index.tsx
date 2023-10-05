3b8b42248984eb959b979c323289fd974c338aca
Jakub Zárybnický <jakub@zarybnicky.com>
2023-10-05 19:36:35 +0200

    chore: fix lint

--- frontend/pages/prijdtancit/index.tsx
+++ frontend/pages/prijdtancit/index.tsx
@@ -2,6 +2,7 @@ import { Layout } from '@/components/layout/Layout';
 import { NextSeo } from 'next-seo';
 import * as React from 'react';
 import { buttonCls } from '@/ui/style';
+import Link from 'next/link';
 import { ChevronRight } from 'lucide-react';
 
 const Page = () => {
@@ -15,21 +16,21 @@ const Page = () => {
 
       <div className="col-feature mb-8 grid lg:grid-cols-2 gap-4">
         <div className="prose prose-accent">
-          <a href="/prijdtancit/mladez"><h2>Děti</h2></a>
+          <Link href="/prijdtancit/mladez"><h2>Děti</h2></Link>
 
           <p>Přípravka tanečního sportu pro děti do 15ti let</p>
           <p>Viděli jste náborovou akci ve škole, reklamu nebo chcete, aby Vaše dítě tancovalo? Pak jste tu správně!</p>
 
-          <a href="/prijdtancit/deti" className={buttonCls({ size: 'lg', className: 'no-underline' })}>Více <ChevronRight /></a>
+          <Link href="/prijdtancit/deti" className={buttonCls({ size: 'lg', className: 'no-underline' })}>Více <ChevronRight /></Link>
         </div>
 
         <div className="prose prose-accent">
-          <a href="/prijdtancit/mladez"><h2>Mládež</h2></a>
+          <Link href="/prijdtancit/mladez"><h2>Mládež</h2></Link>
 
           <p>Taneční kurzy pro mládež</p>
           <p>Máš za sebou taneční? Nebo taneční teprve začínáš a bojíš se, že to nezvládneš? Pak pokračuj do sekce mládež a zjisti víc!</p>
 
-          <a href="/prijdtancit/mladez" className={buttonCls({ size: 'lg', className: 'no-underline' })}>Více <ChevronRight /></a>
+          <Link href="/prijdtancit/mladez" className={buttonCls({ size: 'lg', className: 'no-underline' })}>Více <ChevronRight /></Link>
         </div>
       </div>
     </Layout>
