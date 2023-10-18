cd3c45ed66bd370eec25ee6336447f6274a83b87
Jakub Zárybnický <jakub@zarybnicky.com>
2023-10-18 14:58:50 +0200

    fix: try and fix Calendar in Seznam browser

--- frontend/pages/rozpis.tsx
+++ frontend/pages/rozpis.tsx
@@ -4,6 +4,11 @@ import { Layout } from '@/components/layout/Layout';
 import { NextSeo } from 'next-seo';
 
 const Page = () => {
+
+  if (typeof window !== 'undefined' && navigator.userAgent.includes('Szn')) {
+    window.document.body.style.overscrollBehaviorY = 'contain';
+  }
+
   return (
     <Layout requireMember className="grow overflow-hidden overscroll-contain relative h-[calc(100dvh-68px)] lg:h-[calc(100dvh)]">
       <NextSeo title="Rozpis" />
