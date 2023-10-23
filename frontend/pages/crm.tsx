c5f070ed9f6688ab57ccd906c6b7204a0ffdd95f
Jakub Zárybnický <jakub@zarybnicky.com>
2023-10-23 16:05:10 +0200

    fix: better CRM view

--- frontend/pages/crm.tsx
+++ frontend/pages/crm.tsx
@@ -6,9 +6,33 @@ import { useQuery } from 'urql';
 import { TitleBar } from '@app/ui/TitleBar';
 import { Layout } from '@/components/layout/Layout';
 import { buttonCls } from '@app/ui/style';
+import { cn } from '@/ui/cn';
+import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group';
 
 const Page = () => {
   const [{ data }] = useQuery({query: FormResponsesDocument});
+  const dataset = data?.formResponses?.nodes || [];
+
+  const types = React.useMemo(() => {
+    const types = new Set<string>();
+    dataset.forEach(x => types.add(x.type));
+    return [...types.values()].sort();
+  }, [dataset]);
+
+  const [state, setState] = React.useState('');
+  const currentData = React.useMemo(() => {
+    const selected = state || types[0];
+    return dataset.filter(x => x.type === selected);
+  }, [types, state, dataset]);
+
+  const columns = React.useMemo(() => {
+    const columns = {} as { [key: string]: any };
+    currentData.forEach(x => Object.keys(x.data).forEach(x => {
+      columns[x] = '';
+    }))
+    return Object.keys(columns);
+  }, [currentData]);
+
   const saveData = React.useCallback(
     async (e?: React.MouseEvent) => {
       e?.preventDefault();
@@ -59,42 +83,60 @@ const Page = () => {
 
   return (
     <Layout requireAdmin>
-    <div className="container col-feature">
-      <TitleBar title="Odeslané formuláře">
-        <button type="button" className={buttonCls({ variant: 'outline' })} onClick={saveData}>
-          Export všech
-        </button>
-      </TitleBar>
+      <div className="container col-feature">
+        <TitleBar title="Odeslané formuláře">
+          <button type="button" className={buttonCls({ variant: 'outline' })} onClick={saveData}>
+            Export všech
+          </button>
+        </TitleBar>
 
-      <table className="w-full text-sm text-left text-neutral-11">
-        <thead>
-          <tr>
-            <th>Jméno</th>
-            <th>E-mail</th>
-            <th>Telefon</th>
-            <th>Narození</th>
-            <th>Zdroj</th>
-            <th>Poslední aktivita</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.formResponses?.nodes?.map((row, i) => (
-            <tr key={i} className="even:bg-neutral-2 odd:bg-neutral-1 border-b">
-              <td className="py-1">
-                {row.data.name} {row.data.surname}
-              </td>
-              <td>{row.data.email}</td>
-              <td>{row.data.phone}</td>
-              <td>{row.data.yearofbirth}</td>
-              <td>{row.type}</td>
-              <td className="text-right">
-                {row.createdAt ? fullDateFormatter.format(new Date(row.createdAt)) : ''}
-              </td>
-            </tr>
+        <ToggleGroupPrimitive.Root value={state || types[0]} onValueChange={setState} type="single" className="grow">
+          {types.map(type => (
+            <ToggleGroupPrimitive.Item
+              key={`group-item-${type}`}
+              value={type}
+              className={cn(
+                'group data-[state=on]:bg-accent-5 bg-accent-3 text-accent-11',
+                'border-y px-2 py-1 text-sm first:rounded-l-xl first:border-x last:rounded-r-xl last:border-x',
+                'border-accent-5 data-[state=on]:border-accent-8',
+                'focus:relative focus:outline-none focus-visible:z-30 focus-visible:ring focus-visible:ring-accent-8',
+              )}
+            >
+              {type}
+            </ToggleGroupPrimitive.Item>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </ToggleGroupPrimitive.Root>
+
+        <table className="w-full text-sm text-left text-neutral-12 mt-3">
+          <thead>
+            <tr>
+              {columns.map(x => <th key={x}>{x}</th>)}
+              <th className="text-right">Vytvořeno</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.formResponses?.nodes.filter(x => state ? x.type === state : x.type === types[0])?.map((row, i) => (
+              <tr key={i} className="even:bg-neutral-2 odd:bg-neutral-1 border-b">
+                {columns.map(x => (
+                  <td key={x}>
+                    {x === 'date' ? fullDateFormatter.format(new Date(row.data[x])) : row.data[x]}
+                  </td>
+                ))}
+                {/* <td className="py-1">
+                  {row.data.name} {row.data.surname}
+                </td>
+                <td>{row.data.email}</td>
+                <td>{row.data.phone}</td>
+                <td>{row.data.yearofbirth}</td>
+                */}
+                <td className="text-right">
+                  {row.createdAt ? fullDateFormatter.format(new Date(row.createdAt)) : ''}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
 }
