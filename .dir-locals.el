a859bb72ef08a5bd47fa6b3e73792723f6717dee
Jakub Zárybnický <jakub@zarybnicky.com>
2024-09-04 13:37:37 +0200

    chore: fix broken tsserver

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,17 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
-
 ((typescript-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/"])
-                     (eval . (let
-                                 ((project-directory
-                                   (car
-                                    (dir-locals-find-file default-directory))))
-                               (setq lsp-clients-typescript-server-args
-                                     `("--stdio"))))))
+                     (lsp-clients-typescript-prefer-use-project-ts-server . t)))
  (typescript-tsx-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/"])
-                         (eval . (let
-                                     ((project-directory
-                                       (car
-                                        (dir-locals-find-file default-directory))))
-                                   (setq lsp-clients-typescript-server-args
-                                         `("--stdio")))))))
+                         (lsp-clients-typescript-prefer-use-project-ts-server . t))))
