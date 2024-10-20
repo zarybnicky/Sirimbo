8d210460c96b5227c2566a8e5339d468a9b8e20c
Jakub Zárybnický <jakub@zarybnicky.com>
2024-10-20 14:15:43 +0200

    feat: unregister from events after leaving cohort

--- migrations/current/1-current.sql
+++ migrations/current/1-current.sql
@@ -35,3 +35,71 @@ drop function if exists create_credit_transaction;
 --    and affected.until < event_instance.since
 --    and status = 'unknown'
 -- );
+
+    with affected as (
+      select cohort_membership.until, event_registration.person_id, event_registration.id as registration_id
+      from event_target_cohort
+      join event_registration on event_target_cohort.event_id=event_registration.event_id
+      join cohort_membership on cohort_membership.person_id=event_registration.person_id and cohort_membership.cohort_id=event_target_cohort.cohort_id
+      where cohort_membership.until is not null
+        -- and cohort_membership.id = OLD.id
+    )
+    delete from event_registration
+    where not exists (
+      select event_attendance.id
+      from event_attendance
+      join event_instance on event_instance.id = event_attendance.instance_id
+      join affected on event_attendance.registration_id = affected.registration_id
+       and affected.until < event_instance.since
+       and status <> 'cancelled'
+    );
+
+CREATE or replace FUNCTION app_private.tg_cohort_membership__on_status() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+begin
+  if NEW.status = 'expired' and (TG_OP = 'INSERT' or OLD.status <> NEW.status) then
+
+    with affected as (
+      select cohort_membership.until, event_registration.person_id, event_registration.id as registration_id
+      from event_target_cohort
+      join event_registration on event_target_cohort.event_id=event_registration.event_id
+      join cohort_membership on cohort_membership.person_id=event_registration.person_id and cohort_membership.cohort_id=event_target_cohort.cohort_id
+      where cohort_membership.until is not null
+        and cohort_membership.id = OLD.id
+    )
+    update event_attendance set status = 'cancelled'
+    where id in (
+      select event_attendance.id
+      from event_attendance
+      join event_instance on event_instance.id = event_attendance.instance_id
+      join affected on event_attendance.registration_id = affected.registration_id
+       and affected.until < event_instance.since
+       and status in ('unknown', 'not-excused', 'excused')
+    );
+
+    with affected as (
+      select cohort_membership.until, event_registration.person_id, event_registration.id as registration_id
+      from event_target_cohort
+      join event_registration on event_target_cohort.event_id=event_registration.event_id
+      join cohort_membership on cohort_membership.person_id=event_registration.person_id and cohort_membership.cohort_id=event_target_cohort.cohort_id
+      where cohort_membership.until is not null
+      and cohort_membership.id = OLD.id
+    )
+    delete from event_registration
+    where not exists (
+      select event_attendance.id
+      from event_attendance
+      join event_instance on event_instance.id = event_attendance.instance_id
+      join affected on event_attendance.registration_id = affected.registration_id
+       and affected.until < event_instance.since
+       and status <> 'cancelled'
+    );
+
+  elsif NEW.status = 'active' and (TG_OP = 'INSERT' or OLD.status <> NEW.status) then
+    -- add payments
+    -- add event_registrations to cohort events
+  end if;
+  return NEW;
+end;
+$$;
