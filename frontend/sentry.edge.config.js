12c2404d0572819f63f260182a26270be0b138a4
Jakub Zárybnický <jakub@zarybnicky.com>
2024-11-14 12:18:32 +0100

    Revert "fix @sentry/nextjs warnings"
    This reverts commit fc9e3f00c0b6f5f219fe872df2517912ada6369f.

---
+++ frontend/sentry.edge.config.js
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || 'development',
+  ignoreErrors: [
+    /ResizeObserver loop completed with undelivered notifications/,
+    /attempted to hard navigate to the same URL/,
+  ],
+  dsn:
+    SENTRY_DSN ||
+    'https://943ee3e7e7044524b2ee8413a957e14f@o775093.ingest.sentry.io/5796825',
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
