c9f68fe2c6344aeaaea9672909d57db104946bac
Jakub Zárybnický <jakub@zarybnicky.com>
2025-09-24 11:16:38 +0200

    Disable posthog for now

--- frontend/ui/Tracking.tsx
+++ frontend/ui/Tracking.tsx
@@ -9,7 +9,7 @@ export const Tracking = React.memo(function Tracking() {
   const router = useRouter();
   const auth = useAuth();
   const lastTrackedPath = React.useRef<string>();
-  const posthogRef = React.useRef<PostHog | null>(null);
+  // const posthogRef = React.useRef<PostHog | null>(null);
   const facebookRef = React.useRef<{
     init: typeof init;
     pageView: typeof pageView;
@@ -17,16 +17,16 @@ export const Tracking = React.memo(function Tracking() {
 
   React.useEffect(() => {
     if (process.env.NODE_ENV === 'development') return;
-    (async () => {
-      const posthog = (await import('posthog-js')).posthog;
-      posthog.init('phc_H2WM9q2xXVFl1wEak9TcQVAsOpWNGuauzAvyOmBquYQ', {
-        api_host: `${window.origin}/ingest`,
-      });
-      posthogRef.current = posthog;
-      if (auth.user) {
-        posthogRef.current?.identify(auth.user.uEmail, auth.user);
-      }
-    })();
+    // (async () => {
+    //   const posthog = (await import('posthog-js')).posthog;
+    //   posthog.init('phc_H2WM9q2xXVFl1wEak9TcQVAsOpWNGuauzAvyOmBquYQ', {
+    //     api_host: `${window.origin}/ingest`,
+    //   });
+    //   posthogRef.current = posthog;
+    //   if (auth.user) {
+    //     posthogRef.current?.identify(auth.user.uEmail, auth.user);
+    //   }
+    // })();
     (async () => {
       const facebook = (await import('react-facebook-pixel')).default;
       facebook.init('704526480597551');
@@ -35,17 +35,17 @@ export const Tracking = React.memo(function Tracking() {
     })();
   }, []);
 
-  React.useEffect(() => {
-    if (auth.user) {
-      posthogRef.current?.identify(auth.user.uEmail, auth.user);
-    }
-  }, [auth.user]);
+  // React.useEffect(() => {
+  //   if (auth.user) {
+  //     posthogRef.current?.identify(auth.user.uEmail, auth.user);
+  //   }
+  // }, [auth.user]);
 
   const track = React.useCallback((path: string) => {
     if (process.env.NODE_ENV === 'development') return;
     if (path === lastTrackedPath.current) return;
     lastTrackedPath.current = path;
-    posthogRef.current?.capture('$pageview');
+    // posthogRef.current?.capture('$pageview');
     facebookRef.current?.pageView();
   }, []);
 
