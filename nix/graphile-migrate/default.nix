aaea641d008d8fdeba7435123910c918be719752
Jakub Zárybnický <jakub@zarybnicky.com>
2025-09-28 23:55:03 +0200

    Fix npm audit

--- nix/graphile-migrate/default.nix
+++ nix/graphile-migrate/default.nix
@@ -6,6 +6,6 @@ buildNpmPackage {
   pname = "graphile-migrate";
   version = "2.0.0-rc2";
   src = ./.;
-  npmDepsHash = "sha256-pzzLB0KC1ouq2aRWMSocCLS34DjgWoxmZ/LtoeZ+duQ=";
+  npmDepsHash = "sha256-qZRG2iHrPrAE8tgKnys6MDr1I6rkA4yg3b7f5Gcc84I=";
   dontNpmBuild = true;
 }
