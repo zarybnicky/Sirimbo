619a5b9b6cb4ae222793a64bbdcb8b492b1a4784
Jakub Zarybnicky <kuba.zarybnicky@post.cz>
2025-09-30 07:45:37 +0200

    Add announcement view for renamed upozorneni

--- migrations/current/1-current.sql
+++ migrations/current/1-current.sql
@@ -34,3 +34,49 @@ begin
   end if;
 end;
 $$ language plpgsql;
+
+create or replace view announcement as
+  select
+    up_id as id,
+    tenant_id,
+    up_kdo as author_id,
+    up_nadpis as title,
+    up_text as body,
+    up_lock as is_locked,
+    created_at,
+    updated_at,
+    scheduled_since,
+    scheduled_until,
+    is_visible,
+    sticky as is_sticky,
+    up_timestamp as last_activity_at,
+    up_timestamp_add as published_at
+  from upozorneni;
+
+grant all on announcement to anonymous;
+
+comment on view announcement is E'@primaryKey id
+@foreignKey (tenant_id) references tenant (id)
+@foreignKey (author_id) references users (id)
+@simpleCollections only';
+
+comment on column announcement.id is '@hasDefault';
+comment on column announcement.title is '@notNull';
+comment on column announcement.body is '@notNull';
+comment on column announcement.is_locked is E'@notNull
+@hasDefault';
+comment on column announcement.tenant_id is '@hasDefault';
+comment on column announcement.is_sticky is E'@notNull
+@hasDefault';
+comment on column announcement.created_at is E'@notNull
+@hasDefault';
+comment on column announcement.last_activity_at is E'@notNull
+@hasDefault';
+comment on column announcement.published_at is E'@notNull
+@hasDefault';
+
+comment on table public.upozorneni_skupiny is E'@omit create,update,delete
+@foreignKey (ups_id_rodic) references announcement (id)
+@foreignKey (ups_id_skupina) references cohort (id)';
+comment on constraint upozorneni_skupiny_ups_id_rodic_fkey on upozorneni_skupiny is '@fieldName upozorneni';
+
