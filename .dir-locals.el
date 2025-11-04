f29cc4cf538e022e93a089862f56a9d97bc02bb7
Jakub Zárybnický <jakub@zarybnicky.com>
2025-11-04 07:50:39 +0100

    Prune docs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
-((typescript-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/"])
+((typescript-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/" "csts/" "wdsf/" "worker/"])
                      (lsp-clients-typescript-prefer-use-project-ts-server . t)))
- (tsx-ts-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/"])
+ (tsx-ts-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/" "csts/" "wdsf/" "worker/"])
                      (lsp-clients-typescript-prefer-use-project-ts-server . t)))
- (typescript-tsx-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/"])
+ (typescript-tsx-mode . ((lsp-eslint-working-directories . ["frontend/" "backend/" "csts/" "wdsf/" "worker/"])
                          (lsp-clients-typescript-prefer-use-project-ts-server . t))))
