42e3fdb0fd4e9c96e888fe69b0e161e99b781e01
Jakub Zárybnický <jakub@zarybnicky.com>
2025-12-19 13:11:43 +0100

    Remove post_without_cache

--- backend/src/plugins/proxy.ts
+++ backend/src/plugins/proxy.ts
@@ -7,59 +7,71 @@ const plugins: GraphileConfig.Plugin[] = [
     const executor = build.input.pgRegistry.pgExecutors.main;
 
     return {
-    typeDefs: gql`
-      extend type Query {
-        cstsAthlete(idt: Int!): JSON
-        wdsfAthlete(min: Int!): JSON
-        evidenceStarlet(url: String!, data: JSON!, auth: String): JSON
-      }
-    `,
-    plans: {
-      Query: {
-        cstsAthlete(_$parent: any, args: any) {
-          return withPgClient(executor, args.getRaw('idt'), async (client, idt) => {
-            const { rows: [response] } = await client.query({
-              text: "select content from fetch_with_cache('https://www.csts.cz/api/1/athletes/' || $1, array[http_header('Referrer', 'https://www.csts.cz')])",
-              values: [idt],
+      typeDefs: gql`
+        extend type Query {
+          cstsAthlete(idt: Int!): JSON
+          wdsfAthlete(min: Int!): JSON
+          evidenceStarlet(url: String!, data: JSON!, auth: String): JSON
+        }
+      `,
+      plans: {
+        Query: {
+          cstsAthlete(_$parent: any, args: any) {
+            return withPgClient(executor, args.getRaw('idt'), async (client, idt) => {
+              const {
+                rows: [response],
+              } = await client.query({
+                text: "select content from fetch_with_cache('https://www.csts.cz/api/1/athletes/' || $1, array[http_header('Referrer', 'https://www.csts.cz')])",
+                values: [idt],
+              });
+              return JSON.parse((response as any).content).collection[0];
             });
-            return JSON.parse((response as any).content).collection[0];
-          });
-        },
-        wdsfAthlete(_$parent: any, args: any) {
-          return withPgClient(executor, args.getRaw('min'), async (client, min) => {
-            const url = `https://services.worlddancesport.org/api/1/person/${min}?format=json`;
-            const { rows: [response] } = await client.query({
-              text: "select content from fetch_with_cache($1, array[http_header('Authorization', $2)])",
-              values: [url, process.env.WDSF_AUTH || ''],
-            });
-            return JSON.parse((response as any).content) as any;
-          });
-        },
-        evidenceStarlet(_$parent: any, args: any) {
-          const $args = object({
-            url: args.getRaw('url'),
-            data: args.getRaw('data'),
-            auth: args.getRaw('auth'),
-          });
-          return withPgClient(executor, $args, async function (client, { url, data, auth }: any) {
-            if (auth) {
-              const { rows: [response] } = await client.query({
-                text: "select content from post_without_cache($1, $2, array[http_header('Cookie', 'auth=' || $3)])",
-                values: [url, data, auth],
+          },
+          wdsfAthlete(_$parent: any, args: any) {
+            return withPgClient(executor, args.getRaw('min'), async (client, min) => {
+              const url = `https://services.worlddancesport.org/api/1/person/${min}?format=json`;
+              const {
+                rows: [response],
+              } = await client.query({
+                text: "select content from fetch_with_cache($1, array[http_header('Authorization', $2)])",
+                values: [url, process.env.WDSF_AUTH || ''],
               });
               return JSON.parse((response as any).content) as any;
-            } else {
-              const { rows: [response] } = await client.query({
-                text: "select content from post_without_cache($1, $2, array[]::http_header[])",
-                values: [url, data],
-              });
-              return JSON.parse((response as any).content) as any;
-            }
-          });
+            });
+          },
+          evidenceStarlet(_$parent: any, args: any) {
+            const $args = object({
+              url: args.getRaw('url'),
+              data: args.getRaw('data'),
+              auth: args.getRaw('auth'),
+            });
+            return withPgClient(
+              executor,
+              $args,
+              async function (client, { url, auth, data }: any) {
+                if (auth) {
+                  const {
+                    rows: [response],
+                  } = await client.query({
+                    text: "select content from http('POST', $1, array[http_header('Cookie', 'auth=' || $2)], 'application/json', $3::text)",
+                    values: [url, auth, data],
+                  });
+                  return JSON.parse((response as any).content) as any;
+                } else {
+                  const {
+                    rows: [response],
+                  } = await client.query({
+                    text: "select content from http('POST', $1, array[]::http_header[], 'application/json', $2::text)",
+                    values: [url, data],
+                  });
+                  return JSON.parse((response as any).content) as any;
+                }
+              },
+            );
+          },
         },
       },
-    },
-  };
+    };
   }),
 ];
 
--- migrations/current/1-current.sql
+++ migrations/current/1-current.sql
@@ -1,1 +1,3 @@
+drop type if exists public.prospect_data cascade;
 
+drop function if exists post_without_cache;
