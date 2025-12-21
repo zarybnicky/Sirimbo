5fcd225ff92e3f99379e485d4d00c0e9cb881933
Jakub Zarybnicky <jakub.zarybnicky@oracle.com>
2025-12-21 18:04:03 +0100

    Add missing indices

--- migrations/current/1-current.sql
+++ migrations/current/1-current.sql
@@ -1,1 +1,5 @@
+CREATE INDEX CONCURRENTLY IF NOT EXISTS json_response_frontier_fetched_desc_idx
+  ON crawler.json_response (frontier_id, fetched_at DESC);
 
+CREATE INDEX CONCURRENTLY IF NOT EXISTS html_response_frontier_fetched_desc_idx
+  ON crawler.html_response (frontier_id, fetched_at DESC);
