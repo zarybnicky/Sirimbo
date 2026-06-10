d2981818a5b940ff943968f82f53dc2e16c5312b
Jakub Zárybnický <jakub@zarybnicky.com>
2026-06-10 14:24:39 +0200

    Fix Vercel build

--- frontend/pages/tools/map.tsx
+++ frontend/pages/tools/map.tsx
@@ -9,7 +9,7 @@ export default function EventMap() {
   React.useEffect(() => {
     Promise.all([
       fetch('/cz-admin1.json').then(x => x.json()).then(setCz),
-      fetch('https://api.rozpisovnik.cz/federated/event?federation=eq.csts&start_date=lte.' + new Date().toISOString().slice(0, 10))
+      fetch('https://api.rozpisovnik.cz/federated/event?federation=eq.csts&start_date=lte.' + new Date().toISOString().slice(0, 4) + '-12-31&start_date=gte.' + (Number.parseInt(new Date().toISOString().slice(0, 4)) - 3) + '-12-31')
         .then(x => x.json()).then(setEvents),
     ]);
   }, []);
@@ -18,43 +18,44 @@ export default function EventMap() {
     console.log(cz, events, containerRef);
     if (!cz || !events || !containerRef.current) return;
 
+    const months = ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec'];
     const plot = Plot.plot({
-      width: 1024,
       projection: {
         type: "azimuthal-equidistant",
         domain: cz,
-        inset: 10,
+        inset: 5,
         rotate: [-20],
       },
       axis: null,
       color: {
         type: 'categorical',
         scheme: 'sinebow',
-        domain: [1,2,3,4,5,6,7,8,9,10,11,12],
+        domain: months,
         legend: true,
+      },
+      style: {
+        fontFamily: "Georgia",
       },
       marks: [
         Plot.geo(cz, { stroke: '#aaa' }),
         Plot.dot(events, {
           y: "venue_lat",
           x: "venue_lng",
-          fill: x => Number.parseInt(x.start_date.slice(5, 7)),
+          fill: x => months[Number.parseInt(x.start_date.slice(5, 7)) - 1],
           r: 2,
-          fx: x => (1 + Number.parseInt(x.start_date.slice(0, 4))) % 2,
-          fy: x => Math.floor((1 + Number.parseInt(x.start_date.slice(0, 4))) / 2),
+          fy: x => x.start_date.slice(0, 4),
           channels: {
             title: x => [new Date(x.start_date).toLocaleDateString(), x.name, x.location].join('\n'),
           },
           tip: true,
         }),
-        Plot.text(events, {
-          dx: -20,
+        Plot.text(events, Plot.selectFirst({
+          dx: 20,
           dy: 20,
-          frameAnchor: "top-right",
+          frameAnchor: "top-left",
           text: x => x.start_date.slice(0, 4),
-          fx: x => (1 + Number.parseInt(x.start_date.slice(0, 4))) % 2,
-          fy: x => Math.floor((1 + Number.parseInt(x.start_date.slice(0, 4))) / 2),
-        }),
+          fy: x => x.start_date.slice(0, 4),
+        })),
       ]
     });
     containerRef.current.append(plot);
