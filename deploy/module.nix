550fc7b96c9e9783bd5a00c08510e076fc7dea37
Jakub Zárybnický <jakub@zarybnicky.com>
2026-07-04 02:22:24 +0200

    Fix module

--- deploy/module.nix
+++ deploy/module.nix
@@ -139,6 +139,9 @@ in
         skopeo
         docker
       ];
+      serviceOverrides = {
+        BindReadOnlyPaths = [ "/run/user/1234/docker.sock" ];
+      };
       extraEnvironment = {
         KUBECONFIG = "/etc/rancher/k3s/argocd-ns.yaml:/etc/rancher/k3s/k3s.yaml";
         DOCKER_HOST = "unix:///run/user/1234/docker.sock";
