1b0e41ffc5cfcb3ed493aca99e8ac8f88c533c78
Jakub Zárybnický <jakub@zarybnicky.com>
2026-07-04 02:24:53 +0200

    Debugging the build

--- deploy/module.nix
+++ deploy/module.nix
@@ -140,7 +140,7 @@ in
         docker
       ];
       serviceOverrides = {
-        BindReadOnlyPaths = [ "/run/user/1234/docker.sock" ];
+        BindReadOnlyPaths = [ "/run/user/1234" ];
       };
       extraEnvironment = {
         KUBECONFIG = "/etc/rancher/k3s/argocd-ns.yaml:/etc/rancher/k3s/k3s.yaml";
