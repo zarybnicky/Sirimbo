65d956f850a1b11b5cadbad25c8e2ccd9f9f99a2
Jakub Zarybnicky <jakub.zarybnicky@oracle.com>
2026-07-17 17:03:00 +0200

    Add currentClaims root query

--- frontend/graphql/index.ts
+++ frontend/graphql/index.ts
@@ -5396,6 +5396,7 @@ export type Query = {
   /** Get a single `Couple`. */
   couple: Maybe<Couple>;
   cstsAthlete: Maybe<Scalars['String']['output']>;
+  currentClaims: Maybe<Scalars['JSON']['output']>;
   /** Get a single `EventInstance`. */
   eventInstance: Maybe<EventInstance>;
   /** Get a single `EventInstance`. */
@@ -8670,6 +8671,7 @@ export type GraphCacheResolvers = {
     competitionReportList?: GraphCacheResolver<WithTypename<Query>, QueryCompetitionReportListArgs, Array<WithTypename<CompetitionParticipationRecord> | string>>,
     couple?: GraphCacheResolver<WithTypename<Query>, QueryCoupleArgs, WithTypename<Couple> | string>,
     cstsAthlete?: GraphCacheResolver<WithTypename<Query>, QueryCstsAthleteArgs, Scalars['String']['output'] | string>,
+    currentClaims?: GraphCacheResolver<WithTypename<Query>, Record<string, never>, Scalars['JSON']['output'] | string>,
     eventInstance?: GraphCacheResolver<WithTypename<Query>, QueryEventInstanceArgs, WithTypename<EventInstance> | string>,
     eventInstanceByTenantIdAndId?: GraphCacheResolver<WithTypename<Query>, QueryEventInstanceByTenantIdAndIdArgs, WithTypename<EventInstance> | string>,
     eventInstanceRegistration?: GraphCacheResolver<WithTypename<Query>, QueryEventInstanceRegistrationArgs, WithTypename<EventInstanceRegistration> | string>,
@@ -10235,6 +10237,7 @@ export type GraphCacheUpdaters = {
     competitionReportList?: GraphCacheUpdateResolver<{ competitionReportList: Maybe<Array<WithTypename<CompetitionParticipationRecord>>> }, QueryCompetitionReportListArgs>,
     couple?: GraphCacheUpdateResolver<{ couple: Maybe<WithTypename<Couple>> }, QueryCoupleArgs>,
     cstsAthlete?: GraphCacheUpdateResolver<{ cstsAthlete: Maybe<Scalars['String']['output']> }, QueryCstsAthleteArgs>,
+    currentClaims?: GraphCacheUpdateResolver<{ currentClaims: Maybe<Scalars['JSON']['output']> }, Record<string, never>>,
     eventInstance?: GraphCacheUpdateResolver<{ eventInstance: Maybe<WithTypename<EventInstance>> }, QueryEventInstanceArgs>,
     eventInstanceByTenantIdAndId?: GraphCacheUpdateResolver<{ eventInstanceByTenantIdAndId: Maybe<WithTypename<EventInstance>> }, QueryEventInstanceByTenantIdAndIdArgs>,
     eventInstanceRegistration?: GraphCacheUpdateResolver<{ eventInstanceRegistration: Maybe<WithTypename<EventInstanceRegistration>> }, QueryEventInstanceRegistrationArgs>,
--- migrations/current/1-current.sql
+++ migrations/current/1-current.sql
@@ -3,4 +3,18 @@
 --! include functions/set_event_instance_registration.sql
 --! include functions/set_lesson_demand.sql
 
-drop function if exists federated.competition_sankey_links(integer, integer, text[], text[], boolean);
+drop function if exists federated.competition_sankey_links;
+
+create or replace function public.current_claims() returns jsonb
+    language sql stable security definer
+as $$
+  select to_jsonb(app_private.create_jwt_token(users))
+    - 'exp'
+    - 'is_member'
+    - 'is_trainer'
+    - 'is_admin'
+  from users
+  where id = nullif(current_setting('jwt.claims.user_id', true), '')::bigint;
+$$;
+
+grant all on function public.current_claims() to anonymous;
