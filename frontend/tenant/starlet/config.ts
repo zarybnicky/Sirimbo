f298c57f8266cb53778d225605e1f03dc108dc3c
Jakub Zarybnicky <jakub.zarybnicky@oracle.com>
2026-09-22 15:45:30 +0200

    Starlet favicon

--- frontend/tenant/starlet/config.ts
+++ frontend/tenant/starlet/config.ts
@@ -7,6 +7,17 @@ export const config: TenantConfig = {
     titleTemplate: '%s · TK Starlet',
     defaultTitle: 'TK Starlet',
     themeColor: '#000',
+    additionalLinkTags: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/starlet/apple-touch-icon.png?v=3',
+      },
+      { rel: 'icon', sizes: '96x96', href: '/starlet/favicon-96x96.png?v=3' },
+      { rel: 'icon', href: '/starlet/favicon.svg?v=3' },
+      { rel: 'shortcut icon', href: '/starlet/favicon.ico?v=3' },
+      { rel: 'manifest', href: '/starlet/site.webmanifest?v=3' },
+    ],
   },
   enableRegistration: false,
   enableStarletImport: true,
